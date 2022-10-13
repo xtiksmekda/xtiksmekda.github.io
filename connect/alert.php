22837905121e9553fee0b59b688576812fe22799
xtiksmekda <108170897+xtiksmekda@users.noreply.github.com>
2022-10-13 10:37:44 +0700

    Add files via upload

---
+++ connect/alert.php
@@ -0,0 +1,11 @@
+
+<?php 
+function notice($type)
+{
+	if ($type == 1) {
+		return "<audio autoplay><source src='../assets/music/success.wav'></audio>";
+	} elseif($type == 0) {
+		return "<audio autoplay><source src='../assets/music/error.wav'></audio>";
+	}
+}
+?>
